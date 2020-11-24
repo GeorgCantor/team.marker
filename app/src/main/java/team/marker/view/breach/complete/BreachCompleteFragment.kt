07592f793ba88e239ce9ca7cbcbafdd99c82b275
GeorgCantor <cupsman1986@gmail.com>
2020-11-24 18:41:45 +0300

    fixed photo item grid

--- app/src/main/java/team/marker/view/breach/complete/BreachCompleteFragment.kt
+++ app/src/main/java/team/marker/view/breach/complete/BreachCompleteFragment.kt
@@ -38,22 +38,9 @@ class BreachCompleteFragment : Fragment() {
         for(productId in productIds) {
             Log.e("Message", productId)
         }
-        val size = productIds.size
         var productId = 0
         if (productIds.size > 0) productId = productIds[0].toInt()
-        /*val labelScan = nameCase(size, arrayOf("Отсканирован", "Отсканировано", "Отсканировано"))
-        val labelCode = nameCase(size, arrayOf("код", "кода", "кодов"))
-        if (size > 0) note_text.text = "$labelScan $size $labelCode, введите электронную почту для отправки отчета."
-        else note_text.text = "Отсканированных кодов нет. Выполните повторное сканирование перед отправкой отчета."
-        if (size == 0) {
-            input_email.visibility = View.GONE
-            btn_send.text = "Закрыть"
-            //btn_send.background = ContextCompat.getDrawable(requireContext(), R.drawable.btn_logout)
-            note_title.setImageResource(R.drawable.ic_empty)
-            btn_products.visibility = View.GONE
-            ic_email.visibility = View.GONE
-        }
-        btn_products.setOnClickListener { products(view) }*/
+
         btn_back.setOnClickListener { back(view) }
         btn_send.setOnClickListener { send(view, productId) }
         add_photo_btn.setOnClickListener { findNavController().navigate(R.id.action_breachCompleteFragment_to_photoFragment) }
