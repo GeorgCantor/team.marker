eeebed79f12882b82af17edc89ff765c4d14e0ac
GeorgCantor <cupsman1986@gmail.com>
2020-12-13 19:20:54 +0300

    added removing duplicates

--- app/src/main/java/team/marker/view/pick/PickActivity.kt
+++ app/src/main/java/team/marker/view/pick/PickActivity.kt
@@ -153,6 +153,10 @@ class PickActivity : AppCompatActivity() {
         val quantity = if (quantityRaw.isEmpty()) 0.0 else quantityRaw.toDouble()
         if (quantity <= 0) return
 
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            products.removeIf { it.id == lastId }
+        }
+
         val product = PickProduct()
         product.id = lastId
         product.quantity = quantity
