b7eac4dc989bfb1dfee7cf145c680a16f3f4fa13
GeorgCantor <cupsman1986@gmail.com>
2020-12-31 14:54:23 +0300

    replaced AsyncTask by Coroutines

--- app/src/main/java/team/marker/view/product/WebViewFragment.kt
+++ app/src/main/java/team/marker/view/product/WebViewFragment.kt
@@ -3,18 +3,20 @@ package team.marker.view.product
 import android.app.DownloadManager
 import android.content.Context.DOWNLOAD_SERVICE
 import android.net.Uri
-import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_web_view.*
 import kotlinx.android.synthetic.main.toolbar_file.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import team.marker.R
 import team.marker.util.Constants.PATH
 import team.marker.util.Constants.TITLE
 import team.marker.util.gone
 import java.io.BufferedInputStream
-import java.io.IOException
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
@@ -28,32 +30,23 @@ class WebViewFragment : Fragment(R.layout.fragment_web_view) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        class PdfStream : AsyncTask<String?, Void?, InputStream?>() {
-            override fun doInBackground(vararg strings: String?): InputStream? {
-                var inputStream: InputStream? = null
-                try {
-                    val uri = URL(strings[0])
-                    val urlConnection = uri.openConnection() as HttpURLConnection
-                    if (urlConnection.responseCode == 200) {
-                        inputStream = BufferedInputStream(urlConnection.inputStream)
-                    }
-                } catch (e: IOException) {
-                    return null
-                }
-                return inputStream
-            }
+        GlobalScope.launch(Dispatchers.IO) {
+            var inputStream: InputStream? = null
+            val uri = URL(filePath)
+            val urlConnection = uri.openConnection() as HttpURLConnection
+            if (urlConnection.responseCode == 200) {
+                inputStream = BufferedInputStream(urlConnection.inputStream)
 
-            override fun onPostExecute(inputStream: InputStream?) {
-                super.onPostExecute(inputStream)
-                pdf_screen.fromStream(inputStream)
-                    .defaultPage(0)
-                    .spacing(0)
-                    .fitEachPage(true)
-                    .onLoad { progress_bar.gone() }
-                    .load()
+                withContext(Dispatchers.Main) {
+                    pdf_screen.fromStream(inputStream)
+                        .defaultPage(0)
+                        .spacing(0)
+                        .fitEachPage(true)
+                        .onLoad { progress_bar.gone() }
+                        .load()
+                }
             }
         }
-        PdfStream().execute(filePath)
 
         btn_download.setOnClickListener { downloadFile(filePath, fileTitle) }
         btn_back.setOnClickListener { activity?.onBackPressed() }
