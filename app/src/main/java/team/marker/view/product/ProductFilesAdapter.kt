65f093285a84effd06fb37f25a8f7212d413c025
GeorgCantor <cupsman1986@gmail.com>
2021-01-18 20:32:32 +0300

    simplified ProductFilesAdapter code

--- app/src/main/java/team/marker/view/product/ProductFilesAdapter.kt
+++ app/src/main/java/team/marker/view/product/ProductFilesAdapter.kt
@@ -3,6 +3,8 @@ package team.marker.view.product
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_product_file.view.*
@@ -10,47 +12,43 @@ import kotlinx.android.synthetic.main.item_product_option.view.title_tv
 import team.marker.R
 import team.marker.model.responses.ProductFile
 
-
 class ProductFilesAdapter(
-    options: MutableList<ProductFile>,
+    private val files: MutableList<ProductFile>,
     private val clickListener: (ProductFile) -> Unit
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private val files = mutableListOf<ProductFile>()
-
-    init {
-        this.files.addAll(options)
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val rootView: View = LayoutInflater.from(parent.context).inflate(R.layout.item_product_file, null, false)
-        val lp = RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val lp = RecyclerView.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
         rootView.layoutParams = lp
+
         return ProductOptionsViewHolder(rootView)
     }
 
-    override fun getItemCount(): Int = files.size
+    override fun getItemCount() = files.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val file = files[position]
-        val type = file.type
 
-        holder.itemView.title_tv.text = "${file.title}"
+        with(holder) {
+            itemView.title_tv.text = "${file.title}"
 
-        when (type) {
-            1 -> holder.itemView.icon_iv.setImageResource(R.drawable.ic_pdf)
-            2 -> holder.itemView.icon_iv.setImageResource(R.drawable.ic_word)
-            3 -> holder.itemView.icon_iv.setImageResource(R.drawable.ic_excel)
-            4 -> holder.itemView.icon_iv.setImageResource(R.drawable.ic_txt)
-            5 -> holder.itemView.icon_iv.setImageResource(R.drawable.ic_jpg)
+            itemView.icon_iv.setImageResource(
+                when (file.type) {
+                    1 -> R.drawable.ic_pdf
+                    2 -> R.drawable.ic_word
+                    3 -> R.drawable.ic_excel
+                    4 -> R.drawable.ic_txt
+                    5 -> R.drawable.ic_jpg
+                    else -> R.drawable.ic_pdf
+                }
+            )
+
+            itemView.setOnClickListener { clickListener(file) }
         }
-
-        holder.itemView.setOnClickListener { clickListener(file) }
-
     }
 
     class ProductOptionsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = view.title_tv
     }
-
 }
