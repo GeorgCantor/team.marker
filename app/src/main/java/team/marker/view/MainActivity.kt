bf1c3d9a3911e0cb3dd16b305b8e1fc65ba8be82
GeorgCantor <cupsman1986@gmail.com>
2021-01-21 13:19:03 +0300

    simplified startDestination logic

--- app/src/main/java/team/marker/view/MainActivity.kt
+++ app/src/main/java/team/marker/view/MainActivity.kt
@@ -1,18 +1,15 @@
 package team.marker.view
 
-import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.navigation.NavArgument
 import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 import org.koin.core.qualifier.named
 import team.marker.R
 import team.marker.util.Constants.MAIN_STORAGE
-import team.marker.util.Constants.PRODUCT_URL
 import team.marker.util.Constants.SID
 import team.marker.util.Constants.TOKEN
 import team.marker.util.Constants.access_sid
@@ -33,20 +30,12 @@ class MainActivity : AppCompatActivity() {
         access_sid = preferences.getAny("", SID) as String
         access_token = preferences.getAny("", TOKEN) as String
 
-        val action = intent.action
-        val url = intent.dataString
         val navHostFragment = navHostFragment as NavHostFragment
         val inflater = navHostFragment.navController.navInflater
         val graph = inflater.inflate(R.navigation.nav_graph)
 
-        if (access_token.isEmpty()) {
-            graph.startDestination = R.id.loginFragment
-        } else if (Intent.ACTION_VIEW == action && url != null) {
-            graph.addArgument(PRODUCT_URL, NavArgument.Builder().setDefaultValue(url).build())
-            graph.startDestination = R.id.productFragment
-        } else {
-            graph.startDestination = R.id.homeFragment
-        }
+        graph.startDestination = if (access_token.isEmpty()) R.id.loginFragment else R.id.homeFragment
+
         navHostFragment.navController.graph = graph
 
         getNetworkLiveData(applicationContext).observe(this) {
