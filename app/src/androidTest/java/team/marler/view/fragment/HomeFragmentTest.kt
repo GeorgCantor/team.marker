ed1f109a67382121cee3dd170b7beb99c3245329
GeorgCantor <cupsman1986@gmail.com>
2021-01-24 12:48:21 +0300

    added open_pick_fragment, open_breach_fragment test func

--- app/src/androidTest/java/team/marler/view/fragment/HomeFragmentTest.kt
+++ app/src/androidTest/java/team/marler/view/fragment/HomeFragmentTest.kt
@@ -59,7 +59,25 @@ class HomeFragmentTest : BaseAndroidTest() {
         if (isNetworkAvailable() && isUserLoggedIn()) {
             onView(withId(R.id.btn_scan)).perform(click())
             onView(isRoot()).perform(waitFor(1000))
-            onView(withId(R.id.scan)).check(matches(isDisplayed()))
+            onView(withId(R.id.btn_scan_flash)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun open_pick_fragment() {
+        if (isNetworkAvailable() && isUserLoggedIn()) {
+            onView(withId(R.id.btn_pick)).perform(click())
+            onView(isRoot()).perform(waitFor(1000))
+            onView(withId(R.id.pick_toolbar)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun open_breach_fragment() {
+        if (isNetworkAvailable() && isUserLoggedIn()) {
+            onView(withId(R.id.btn_breach)).perform(click())
+            onView(isRoot()).perform(waitFor(1000))
+            onView(withId(R.id.sv_barcode)).check(matches(isDisplayed()))
         }
     }
 }
