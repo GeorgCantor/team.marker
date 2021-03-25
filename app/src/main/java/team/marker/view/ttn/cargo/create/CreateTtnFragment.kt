1993096930698b670450112f50a878f8133e91ac
GeorgCantor <cupsman1986@gmail.com>
2021-03-25 20:25:26 +0300

    Add inputs, spinners

--- app/src/main/java/team/marker/view/ttn/cargo/create/CreateTtnFragment.kt
+++ app/src/main/java/team/marker/view/ttn/cargo/create/CreateTtnFragment.kt
@@ -2,7 +2,9 @@ package team.marker.view.ttn.cargo.create
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.core.content.ContextCompat.getColor
 import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
@@ -27,20 +29,26 @@ class CreateTtnFragment : Fragment(R.layout.fragment_create_ttn) {
         val listInvoice = ExpandList(expand_invoice, exp_icon_invoice)
         val listTransport = ExpandList(expand_transport, exp_icon_transport)
         val listShipper = ExpandList(expand_shipper, exp_icon_shipper)
-        val listConsignee = ExpandList(expand_consignee, exp_icon_consignee)
-        val listPayer = ExpandList(expand_payer, exp_icon_payer)
         listInvoice.toggleList(FORCE)
 
         btn_back.setOnClickListener { activity?.onBackPressed() }
         item_invoice.setOnClickListener { listInvoice.toggleList() }
         item_transport.setOnClickListener { listTransport.toggleList() }
         item_shipper.setOnClickListener { listShipper.toggleList() }
-        item_consignee.setOnClickListener { listConsignee.toggleList() }
-        item_payer.setOnClickListener { listPayer.toggleList() }
 
         inputs = listOf(
-            input_invoice, input_transport, input_shipper, input_consignee, input_payer
+            input_freight_cost,
+            input_shipping_cost,
+            input_gross_weight
         ).onEach { it.doAfterTextChanged { checkAllFilled() } }
+
+        val spinners = listOf(
+            ts_spinner to R.array.ts_type_array,
+            shipper_spinner to R.array.companies_array,
+            customer_spinner to R.array.companies_array,
+            carrier_spinner to R.array.companies_array,
+            consignee_spinner to R.array.companies_array
+        ).onEach { setSpinner(it.first, it.second) }
 
         viewModel.progressIsVisible.observe(viewLifecycleOwner) { progress_bar.isVisible = it }
 
@@ -57,6 +65,15 @@ class CreateTtnFragment : Fragment(R.layout.fragment_create_ttn) {
             btn_send.isClickable = this
             btn_send.setBackgroundColor(getColor(requireContext(), if (this) R.color.dark_blue else R.color.gray))
         }
+    }
+
+    private fun setSpinner(spinner: Spinner, resArray: Int) = ArrayAdapter.createFromResource(
+        requireContext(),
+        resArray,
+        android.R.layout.simple_spinner_item
+    ).also { adapter ->
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 
     override fun onDestroyView() {
